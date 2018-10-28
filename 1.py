36d67ec983ab78befd07d649f05d0fcbbd7f289e
terpsihora96 <dzedaj996@gmail.com>
2018-10-28 21:53:07 +0100

    Code polished.

--- 1.py
+++ 1.py
@@ -1,17 +1,16 @@
 import sys
 
 def main():
-    numbers = [2, 15, 23, 3, 4]
-    k = 17
-
-    elements = dict()
+    numbers = [-2, -15, 23, 3, 4]
+    k = -17
+    dict_numbers = dict()
+    
     for number in numbers:
-        y = k - number
-        x = elements.get(y, False)
-        if (x == False):
-            elements[number] = k - number
+        v = k - number
+        if dict_numbers.get(v, False):
+            dict_numbers[number] = v
         else:
-            print(k - number, number)
+            print(f'({v}) + ({number}) = {k}')
             sys.exit(0) 
 
 if __name__ == "__main__":
