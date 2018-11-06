a8a5e94c5fc62b2000652826d7dcd92744fdbf19
terpsihora96 <dzedaj996@gmail.com>
2018-11-06 23:44:37 +0100

    Text of the task added.

--- 2.py
+++ 2.py
@@ -1,3 +1,7 @@
+#Given an array of integers, return a new array such that 
+# each element at index i of the new array is 
+# the product of all the numbers in the original array except the one at i.
+
 from functools import reduce
 # from copy import deepcopy
 
